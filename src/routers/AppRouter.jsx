3847fce2a0df83d6a7ba183741dcb9b6bcfe2536
leti <mendietaletizia@gmail.com>
2025-10-11 21:01:41 -0400

    Implementacion de la bitacora

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -14,6 +14,7 @@ import OrdenPage from "../pages/ordenes/OrdenPage.jsx";
 import OrdenDetalle from "../pages/ordenes/OrdenDetalle.jsx";
 import PresupuestoPage from "../pages/presupuestos/PresupuestoPage.jsx";
 import PresupuestoDetalle from "../pages/presupuestos/PresupuestoDetalle.jsx";
+import BitacoraPage from "../pages/bitacora/BitacoraPage.jsx";
 
 const AdminRoutes = () => {
   const isAuthenticated = !!localStorage.getItem("access");
@@ -57,6 +58,7 @@ const AppRouter = () => {
           <Route path="/admin/empleados" element={<EmpleadoPage />} />
           <Route path="/admin/clientes" element={<ClientePage />} />
           <Route path="/admin/roles" element={<RolePage />} />
+          <Route path="/admin/bitacora" element={<BitacoraPage />} />
           <Route path="/ordenes" element={<OrdenPage />} />
           <Route path="/ordenes/:id" element={<OrdenDetalle />} />
           <Route path="/presupuestos" element={<PresupuestoPage />} />
